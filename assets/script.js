90a4075ceb21c135474041df7076381cbec312d6
Josh <lutz_143@yahoo.com>
2022-11-25 15:11:13 -0500

    cleaned up code and added comments

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,47 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 // capture relevant elements for future rendering
 var currentTimeEl = $("#currentDay");
 var hourContainerEl = $("#time-container");
-var timeNodeEl = $("#hour-9");
 var taskLog = JSON.parse(window.localStorage.getItem('taskLog')) || [];
 
-let workMilitaryStart = 0;
+// set the start and end time for the planner in military time
+let workMilitaryStart = 7;
 let workMilitaryEnd = 18;
 
+// capture today's date and time now utilizing dayjs() and formatting accordingly
 const today = dayjs().format('dddd, MMMM D, YYYY');
-const testTime = dayjs('2022-11-16T20:00:00');
-const testTime2 = dayjs('2022-11-16T22:00:00');
 const timeNow = dayjs().format('MM/DD/YYYY HH:mm A');
 
 // create function to render current time at top of the page
 function displayTimeNow(){
   currentTimeEl.text(today);
-
   hourContainerEl.append()
 }
 
-
+// create a function that will render the start and end times
 function workHours() {
+  // set the military start and end time to 0 minutes and 0 seconds in order to iterate
   var workStart = dayjs().hour(workMilitaryStart).minute(0).second(0);
   var workEnd = dayjs().hour(workMilitaryEnd).minute(0).second(0);  
+  // capture the number of hours between work start and work finish
   var workHours = workEnd.diff(workStart, 'hours');
   workHour = [workMilitaryStart-1];
 
+  // iterate over the number of work hours, format and display in the daily planner
   for(var i=0; i < workHours; i++) {
     workHour++;
     var workHourFormat = dayjs().hour(workHour).minute(0).second(0);
     var workCurrentHour = workHourFormat.format('H');
     var workHourFormat = workHourFormat.format('ha');
 
+    // capture the relevant elements to render the planner
     var timeNodeEl = document.createElement('div');
     var timeNode = document.createElement('div');
     var textAreaEl = document.createElement('textarea');
     var saveBtn = document.createElement('button');
     var iEl = document.createElement('i');
 
+    // compare the work hour in the planner to the current hour and set the CSS attribute
+    // to either a past styling, current styling, or future styling
     if (dayjs().hour() > (workCurrentHour)) {
       timeNodeEl.setAttribute('class', 'row time-block past');
     } else if (dayjs().hour() == workCurrentHour) {
@@ -50,6 +50,7 @@ function workHours() {
       timeNodeEl.setAttribute('class', 'row time-block future');
     }
 
+    // set the attributes for styling of the daily planner
     timeNodeEl.setAttribute('id', workHourFormat);
     timeNode.setAttribute('id', 'taskHour');
     timeNode.setAttribute('class', 'col-2 col-md-1 hour text-center py-3');
@@ -61,8 +62,8 @@ function workHours() {
     iEl.setAttribute('class', 'fas fa-save');
     iEl.setAttribute('aria-hidden', 'true');
 
+    // show the hour and append to render on screen
     timeNode.textContent = workHourFormat;
-
     hourContainerEl.append(timeNodeEl);
     timeNodeEl.append(timeNode);
     timeNodeEl.append(textAreaEl);
@@ -97,6 +98,7 @@ function workHours() {
   }
 }
 
+// create an on click function to capture the task and the task hour and save to localStorage
 $(document).ready(function() {
   $(".saveBtn").on("click", function(event){
     event.preventDefault();
@@ -107,6 +109,7 @@ $(document).ready(function() {
   })
 })
 
+// create an on click function to clear localStorage
 $(document).ready(function() {
   $("#clearStorage").on("click", function(){
     localStorage.clear();
@@ -117,25 +120,7 @@ $(document).ready(function() {
 function handleFormSubmit(event) {
   event.preventDefault();
 
-  $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+  $(function () {    
   });
 }
 
